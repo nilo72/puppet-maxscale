afbaf6911e425227106ba0ce7d97738f949d2b45
Oliver Neumann <oliver.neumann@haw-hamburg.de>
2016-06-10 13:53:34 +0200

    params enhanced with distro-switch and more variables. use key server than source...

--- manifests/install.pp
+++ manifests/install.pp
@@ -4,15 +4,14 @@ class maxscale::install inherits maxscale {
   
   apt::source { 'maxscale':
     location => $maxscale::apt_url,
-    release  => 'jessie',
-    repos    => 'main',
+    release  => $release,
+    repos    => $repository,
     key      => {
-      id     => '199369E5404BD5FC7D2FE43BCBCB082A1BB943DB',
-      source => 'http://downloads.mariadb.com/enterprise/xagz-pk9v/mariadb-maxscale/MariaDB-MaxScale-GPG-KEY',
+      id     => $fingerprint,
+      server => $keyserver,
     },
   }
-  
-  
+
   package { 'maxscale':
     ensure  => present,
     require => Apt::Source['maxscale'],
--- manifests/params.pp
+++ manifests/params.pp
@@ -3,5 +3,17 @@ class maxscale::params {
   $service_ensure = 'running'
   $service_enable = true
   $service_name = 'maxscale'
-  $apt_url = 'http://example.com/apt/test'
+  $apt_url = undef
+
+  case $::osfamily {
+    'Debian': {
+      $release     = $::lsbdistcodename
+      $repository  = 'main'
+      $fingerprint = '13CFDE6DD9EE9784F41AF0F670E4618A8167EE24'
+      $keyserver   = 'hkp://keyserver.ubuntu.com:80'
+    }
+    default: {
+      fail("${::operatingsystem} not supported")
+    }
+  }
 }
