16812702734f61cd8c82f0fecfcedf95ae347317
Oliver Neumann <oliver.neumann@haw-hamburg.de>
2016-06-10 13:12:47 +0200

    wrong id

--- manifests/install.pp
+++ manifests/install.pp
@@ -7,7 +7,7 @@ class maxscale::install inherits maxscale {
     release  => 'jessie',
     repos    => 'main',
     key      => {
-      id     => '﻿13CFDE6DD9EE9784F41AF0F670E4618A8167EE24',
+      id     => '13CFDE6DD9EE9784F41AF0F670E4618A8167EE24',
       source => 'http://downloads.mariadb.com/enterprise/xagz-pk9v/mariadb-maxscale/latest/debian/dists/jessie/Release.gpg',
     },
   }
