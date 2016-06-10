a600a63b251ed73b6b2f9f8c0c39675b9eb8d497
Oliver Neumann <oliver.neumann@haw-hamburg.de>
2016-06-10 13:30:11 +0200

    key riddle

--- manifests/install.pp
+++ manifests/install.pp
@@ -7,7 +7,7 @@ class maxscale::install inherits maxscale {
     release  => 'jessie',
     repos    => 'main',
     key      => {
-      id     => '13CFDE6DD9EE9784F41AF0F670E4618A8167EE24',
+      id     => '199369E5404BD5FC7D2FE43BCBCB082A1BB943DB',
       source => 'http://downloads.mariadb.com/enterprise/xagz-pk9v/mariadb-maxscale/latest/debian/dists/jessie/Release.gpg',
     },
   }
